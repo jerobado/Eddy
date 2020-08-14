385a62b9ada581df69809d5a94c1cb4b5cdb601f
Jero Bado <tokidokitalkyou@gmail.com>
2020-08-14 12:09:54 +0800

    Implemented/added a menu bar
    Added initial 'File' and 'Quit' menu. Signal and Slots not yet connected.

--- src/window.cpp
+++ src/window.cpp
@@ -4,6 +4,11 @@ Window::Window(QMainWindow *parent) : QMainWindow(parent)
 {
     // widgets
     eddyPlainTextEdit = new QPlainTextEdit();
+    
+
+    // actions and menus
+    createActions();
+    createMenus();
 
 
     // properties
@@ -14,4 +19,19 @@ Window::Window(QMainWindow *parent) : QMainWindow(parent)
     setCentralWidget(eddyPlainTextEdit);
 
     // connections
+}
+
+void Window::createActions()
+{
+    newAction = new QAction(tr("&New"), this);
+    quitAction = new QAction(tr("&Quit"), this);
+    // [] TODO: set signal and slots
+}
+
+
+void Window::createMenus()
+{
+    fileMenu = menuBar()->addMenu(tr("&File"));
+    fileMenu->addAction(newAction);
+    fileMenu->addAction(quitAction);
 }
