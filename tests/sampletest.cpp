e38579f502c16406c214505b8c42adac6e218282
Jero Bado <tokidokitalkyou@gmail.com>
2020-10-27 19:53:49 +0800

    Added new macros to test default properties

--- tests/sampletest.cpp
+++ tests/sampletest.cpp
@@ -8,6 +8,8 @@ class TestEddy: public QObject
     private:
         Window test_eddyWindow;
         QString windowTitle = "Eddy 0.1.14";
+        QString appName = "Eddy";
+        QString appVersion = "0.1.14";
     
     private slots:
         void test_default_properties();
@@ -16,7 +18,15 @@ class TestEddy: public QObject
 
 void TestEddy::test_default_properties()
 {
+    QSettings settings("Mokachokokarbon", appName);    
+
     QCOMPARE(test_eddyWindow.windowTitle(), QString(windowTitle));
+    QCOMPARE(test_eddyWindow.appName, QString(appName));
+    QCOMPARE(test_eddyWindow.appVersion, QString(appVersion));
+    QCOMPARE(test_eddyWindow.lastKnownFilePath, settings.value("lastKnownFilePath").toString());
+    QCOMPARE(test_eddyWindow.timestampFormat, QString("h:mm:ss ap, ddd, d MMM yyyy"));
+    QCOMPARE(test_eddyWindow.isEddyPlainTextEditTextChanged, false);
+    QCOMPARE(test_eddyWindow.isNewFile, false);
 }
 
 
